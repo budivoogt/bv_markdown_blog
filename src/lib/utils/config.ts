bf55536ff9b5b56bfd3c1f35c3fb46e0ab03c963
brucey0x <11062155+brucey0x@users.noreply.github.com>
2024-04-25 16:31:36 +0200

    Updated Open Graph photo to one a bit more serious

--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -1,4 +1,4 @@
 export const siteTitle: string = "Budi Voogt"
 export const siteDescription: string =
 	"From music industry founder to angel investor and indie web developer. Looking to bootstrap my next business. Follow my journey on this blog."
-export const openGraphImage = {src: "bv-blog/budi_headshot", alt: "Headshot of Budi Voogt"}
+export const openGraphImage = { src: "bv-blog/budi_headshot_2", alt: "Headshot of Budi Voogt" }
